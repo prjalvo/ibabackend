2124f520985f97e1cac3a3849a38ba66223c5690
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2022-10-15 23:57:24 -0300

    Update auth.router.js

--- src/api/resources/auth/auth.router.js
+++ src/api/resources/auth/auth.router.js
@@ -6,7 +6,7 @@ import { validateBody, schemas } from '../../../middleware/validator.js';
 
 export const authRouter = express.Router();
 authRouter.route('/register').post(sanitize(),/* validateBody(schemas.registerSchema), */ authController.addUser);
-authRouter.route('/user/getAllUserList').get(sanitize(), jwtStrategy, authController.getAllUserList);
+authRouter.route('/user/getAllUserList').get(sanitize(),authController.getAllUserList);
 authRouter.route('/user/update').post(sanitize(), jwtStrategy, authController.userUpdate);
 authRouter.route('/user/delete').post(sanitize(), jwtStrategy, authController.deleteUserList);
 authRouter.route('/getUserByEmailId').get( authController.findUser);

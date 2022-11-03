a988c56c154bfb4d1238921415e4350736883215
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2022-11-02 21:47:29 -0300

    Update order.controller.js

--- src/api/resources/order/order.controller.js
+++ src/api/resources/order/order.controller.js
@@ -66,25 +66,9 @@ export default {
       
         try {
             db.formularios.findAll({
-                attributes: ['id_celula',
-                 [Sequelize.fn('SUM', Sequelize.col('membresia')), 'total_membresia'],                 
-                 [Sequelize.fn('SUM', Sequelize.col('batismo')), 'total_batismo'],
-                 [Sequelize.fn('SUM', Sequelize.col('lider_celula')), 'total_lider_celula'],
-                 [Sequelize.fn('SUM', Sequelize.col('disc_superv')), 'total_disc_superv'],
-                 [Sequelize.fn('SUM', Sequelize.col('kids_incl')), 'total_kids_incl'],
-                 [Sequelize.fn('SUM', Sequelize.col('intr_b_nt')), 'total_intr_b_nt'],
-                 [Sequelize.fn('SUM', Sequelize.col('ant_test')), 'total_ant_test'],
-                 [Sequelize.fn('SUM', Sequelize.col('batalha_esp')), 'total_batalha_esp'],
-                 [Sequelize.fn('SUM', Sequelize.col('tetelestai')), 'total_tetelestai'],
-                 [Sequelize.fn('SUM', Sequelize.col('vida')), 'total_vida'],
-                 [Sequelize.fn('SUM', Sequelize.col('tsd')), 'total_tsd'],
-                 [Sequelize.fn('SUM', Sequelize.col('discipulado')), 'total_discipulado'],           
-                 
-            ], 
-                
-               include: [{ model: db.grupos, attributes: ["id", "descricao","id_lider","id_colider","id_supervisor","id_setor","id_area","id_distrito"] }
-               ],
-                
+                attributes: ['id_celula',"membresia","batismo","lider_celula","disc_superv","kids_incl","intr_b_nt","intr_b_nt","ant_test","batalha_esp","tetelestai","vida","tsd","discipulado"],                 
+                include: [{ model: db.grupos, attributes: ["id", "descricao","id_lider","id_colider","id_supervisor","id_setor","id_area","id_distrito"] }
+               ],                
             })
                 .then(list => {
                     res.status(200).json({ 'success': true, data: list });

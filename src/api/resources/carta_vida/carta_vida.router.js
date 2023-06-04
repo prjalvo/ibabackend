a3f63e1f0341eace5ef4ec14e0b304a1be2f0e06
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-03 22:18:41 -0300

    Update carta_vida.router.js

--- src/api/resources/carta_vida/carta_vida.router.js
+++ src/api/resources/carta_vida/carta_vida.router.js
@@ -3,10 +3,10 @@ import carta_vidaController from './cargo.controller.js';
 import { sanitize } from '../../../middleware/sanitizer.js';
 import { jwtStrategy } from '../../../middleware/strategy.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
-import { s3, bucket, upload } from "../../../middleware/bucket.js";
+
 
 export const carta_vidaRouter = express.Router();
-carta_vidaRouter.route('/upload').post(upload.single('file'), carta_vidaController.uploadController);
+carta_vidaRouter.route('/upload').post(carta_vidaController.uploadController);
 
 
 

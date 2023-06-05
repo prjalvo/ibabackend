a891da68f0358451cead02315d07ed39e0db1262
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-04 22:59:35 -0300

    Update cargo.router.js

--- src/api/resources/carta_vida/cargo.router.js
+++ src/api/resources/carta_vida/cargo.router.js
@@ -8,9 +8,6 @@ import { s3, bucket, upload } from "../../../middleware/bucket.js";
 export const cargoRouter = express.Router();
 cargoRouter.route('/create').post(sanitize(), jwtStrategy, cargoController.index);
 cargoRouter.route('/list').get(sanitize(),cargoController.List);
-cargoRouter.route('/upload').get(upload.single("file"), (req, res) => {
-    return res.json({ message: req.file.location });
-  });
 cargoRouter.route('/delete').delete(sanitize(),jwtStrategy,cargoController.getcargoDelete);
 cargoRouter.route('/update').post(sanitize(),jwtStrategy,cargoController.getcargoUpdate);
 cargoRouter.route('/getCargoById').get(sanitize(), cargoController.getCargoListById);

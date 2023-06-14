a70bb826da5f87ec7540bbe683dde292a3185903
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-14 08:55:56 -0300

    Create Visita_distrito.cjs

---
+++ src/models/Visita_distrito.cjs
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const visita_distrito = sequelize.define('visita_distrito', {
+    desc_nome: DataTypes.STRING,
+    desc_cargo: DataTypes.STRING,
+    desc_celula: DataTypes.STRING, 
+    id_lider: DataTypes.INTEGER,
+    id_cargo: DataTypes.INTEGER, 
+    id_celula: DataTypes.INTEGER,
+    JanFevMar: DataTypes.INTEGER,
+    AbrMaiJun: DataTypes.INTEGER,
+    JulAgoSet: DataTypes.INTEGER,
+    OutNovDez: DataTypes.INTEGER,
+  },
+   {
+     tableName: 'visita_distrito',
+     timestamps: false, // Se a view não tiver colunas de data de criação/atualização, desative os timestamps
+   },                                      
+  {});
+
+  visita_distrito.associate = function(models) {     
+  };
+
+  return visita_distrito;
+};

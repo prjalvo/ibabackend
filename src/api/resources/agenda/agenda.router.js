a56a837e0fcafaff02a0e2534d13252802882249
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-14 09:02:02 -0300

    Update agenda.router.js

--- src/api/resources/agenda/agenda.router.js
+++ src/api/resources/agenda/agenda.router.js
@@ -11,6 +11,10 @@ agendaRouter.route('/update').post(agendaController.getAgendaUpdate);
 agendaRouter.route('/getAllAgendaList').get(sanitize(), agendaController.getAllAgendaList);
 agendaRouter.route('/delete').delete(sanitize(), agendaController.getAgendaDelete);
 
+agendaRouter.route('/getVisita_Setor').get(sanitize(), agendaController.getVisita_Setor);
+agendaRouter.route('/getVisita_Area').get(sanitize(), agendaController.getVisita_Area);
+agendaRouter.route('/getVisita_Distrito').get(sanitize(), agendaController.getVisita_Distrito);
+
 
 
 

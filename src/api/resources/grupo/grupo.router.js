b6121cc60b316b55cde59346dc559656b7ba76fc
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-20 09:27:05 -0300

    Update grupo.router.js

--- src/api/resources/grupo/grupo.router.js
+++ src/api/resources/grupo/grupo.router.js
@@ -9,7 +9,7 @@ export const grupoRouter = express.Router();
 grupoRouter.route('/add').post(jwtStrategy, grupoController.addGrupo);
 grupoRouter.route('/getAllGrupoList').get(sanitize(), grupoController.getAllGrupoList);
 grupoRouter.route('/getAllLider').get(sanitize(), grupoController.getAllLider);
-grupoRouter.route('/update').post(sanitize(), grupoController.update);
+grupoRouter.route('/update').post(grupoController.update);
 //pessoaRouter.route('/getProductByCategory').get(sanitize(), productController.getProductListByCategory);
 grupoRouter.route('/getGrupoById').get(sanitize(), grupoController.getGrupoListById);
 grupoRouter.route('/delete').delete(sanitize(), grupoController.grupoDelete);

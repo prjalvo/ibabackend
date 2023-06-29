33d18e2b3b6f5e11141e27b363c83dda3352733d
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-06-29 10:17:05 -0300

    Update grupo_membros.controller.js

--- src/api/resources/grupo_membros/grupo_membros.controller.js
+++ src/api/resources/grupo_membros/grupo_membros.controller.js
@@ -92,50 +92,52 @@ export default {
             throw new RequestError('Error');
         }
     },
-
     async getAllGrupoMembrosFormula(req, res, next) {
-   
-        try {
-            db.formularios.findAll({                
-                order: [['createdAt', 'DESC']],     
-                include: [                                                     
-                { model: db.grupo_membros, attributes: ["id", "nome","telefone","email","data_conversao"] },
-                { model: db.grupos, attributes: ["id", "descricao"],
-                  include: [
-                          {
-                            model: db.users,
-                            attributes: ["id_area","id_setor","id_distrito","id_lider"],
-                            where: { id: db.sequelize.col('grupos.id_lider') },
-                            required: true,
-                               include: [
-                                          {
-                                            model: db.areas,
-                                            attributes: ["descricao","tipo"],
-                                            where: {
-                                                      [db.sequelize.Op.or]: [
-                                                        { id: db.sequelize.col('users.id_area') },
-                                                        { id: db.sequelize.col('users.id_distrito') },
-                                                        { id: db.sequelize.col('users.id_setor') }
-                                                      ]
-                                                    }  
-                                          }
-                                        ]
-                          }                      
-                        ],
-                 ]
-            })
-                .then(formularios => {
-                    res.status(200).json({ 'success': true, formularios });
-                })
-                .catch(function (err) {
-                    next(err)
-                });
+    try {
+    db.formularios.findAll({
+      order: [['createdAt', 'DESC']],
+      include: [
+        {
+          model: db.grupo_membros,
+          attributes: ["id", "nome", "telefone", "email", "data_conversao"]
+        },
+        {
+          model: db.grupos,
+          attributes: ["id", "descricao"],
+          include: [
+            {
+              model: db.users,
+              attributes: ["id_area", "id_setor", "id_distrito", "id_lider"],
+              where: { id: db.sequelize.col('grupos.id_lider') },
+              required: true,
+              include: [
+                {
+                  model: db.areas,
+                  attributes: ["descricao", "tipo"],
+                  where: {
+                    [db.sequelize.Op.or]: [
+                      { id: db.sequelize.col('users.id_area') },
+                      { id: db.sequelize.col('users.id_distrito') },
+                      { id: db.sequelize.col('users.id_setor') }
+                    ]
+                  }
+                }
+              ]
+            }
+          ]
         }
-        catch (err) {
-            throw new RequestError('Error');
-        }
-    },
-    
+      ]
+    })
+      .then(formularios => {
+        res.status(200).json({ 'success': true, formularios });
+      })
+      .catch(function (err) {
+        next(err)
+      });
+  } catch (err) {
+    throw new RequestError('Error');
+  }
+},
     async update(req, res, next) {
         try {
             const { id,nome, status,telefone,email,info,data_nascimento,data_conversao,id_ministerio } = req.body;  

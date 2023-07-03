9fbd14b3e60902d8aa17f503efc0a9ccb3044ab2
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-07-02 22:36:24 -0300

    Update batismo.router.js

--- src/api/resources/batismo/batismo.router.js
+++ src/api/resources/batismo/batismo.router.js
@@ -5,5 +5,5 @@ import { sanitize } from '../../../middleware/sanitizer.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
 
 export const batismoRouter = express.Router();
-batismoRouter.route('/register').post(batismoController.addBatismo);
+batismoRouter.route('/batismo/register').post(batismoController.addBatismo);
 batismoRouter.route('/batismo/getAllBatismoList').get(sanitize(), jwtStrategy, batismoController.getAllBatismoList);

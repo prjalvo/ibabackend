91eae2cf2168d2fab23930546b2f87588c996743
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-10-13 15:02:04 -0300

    Update User_cargo.cjs

--- src/models/User_cargo.cjs
+++ src/models/User_cargo.cjs
@@ -3,7 +3,11 @@ module.exports = (sequelize, DataTypes) => {
   const user_cargo = sequelize.define('user_cargo', {
     id_cargo: DataTypes.INTEGER,
     id_user: DataTypes.INTEGER,    
-  },    
+  },
+  {
+     tableName: 'user_cargo',
+     timestamps: false, // Se a view não tiver colunas de data de criação/atualização, desative os timestamps
+   },   
   {});
  
   user_cargo.associate = function(models) {   

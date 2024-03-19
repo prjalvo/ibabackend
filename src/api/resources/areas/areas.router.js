59a46cd4c00442232d1b90b0d12037198bc04f6d
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-03-19 19:52:22 -0300

    Update areas.router.js

--- src/api/resources/areas/areas.router.js
+++ src/api/resources/areas/areas.router.js
@@ -6,7 +6,7 @@ import { validateBody, schemas } from '../../../middleware/validator.js';
 
 export const areasRouter = express.Router();
 areasRouter.route('/create').post(sanitize(), jwtStrategy,areasController.index);
-areasRouter.route('/list').get(sanitize(),areasController.List);
+areasRouter.route('/list').get(sanitize(),jwtStrategy,areasController.List);
 areasRouter.route('/delete').delete(sanitize(),jwtStrategy,areasController.getareasDelete);
 areasRouter.route('/update').post(sanitize(),jwtStrategy,areasController.getareasUpdate);
 areasRouter.route('/getareasById').get(sanitize(), areasController.getareasListById);

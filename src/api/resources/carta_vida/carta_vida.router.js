200a0d6065966ddd9462e323a85c3dba913cbfe8
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-09 16:29:42 -0300

    Update carta_vida.router.js

--- src/api/resources/carta_vida/carta_vida.router.js
+++ src/api/resources/carta_vida/carta_vida.router.js
@@ -8,6 +8,6 @@ import { validateBody, schemas } from '../../../middleware/validator.js';
 export const carta_vidaRouter = express.Router();
 carta_vidaRouter.route('/create').post(carta_vidaController.index);
 carta_vidaRouter.route('/delete').delete(sanitize(),jwtStrategy,carta_vidaController.getcartaDelete);
-carta_vidaRouter.route('/update').post(sanitize(),jwtStrategy,carta_vidaController.getcartaUpdate);
+carta_vidaRouter.route('/update').post(jwtStrategy,carta_vidaController.getcartaUpdate);
 carta_vidaRouter.route('/getCartaById').get(carta_vidaController.getCartaListById);
 carta_vidaRouter.route('/list').get(carta_vidaController.List);

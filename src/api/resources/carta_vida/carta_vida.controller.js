68249532b6fe08b7e5f361d4ed894af1da8be996
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-12 17:51:04 -0300

    Update carta_vida.controller.js

--- src/api/resources/carta_vida/carta_vida.controller.js
+++ src/api/resources/carta_vida/carta_vida.controller.js
@@ -4,7 +4,15 @@ import { s3, bucket, upload } from "../../../middleware/bucket.js";
 export default {
    async index(req, res, next) {
         try {
-            const { texto,id_participante,nome } = req.body;
+            let { texto,id_participante,nome } = req.body;
+             function removeHtmlTags(str) 
+             {
+               return str.replace(/<\/?[^>]+(>|$)/g, "");
+             }
+
+            // Remove HTML tags do campo texto
+            texto = removeHtmlTags(texto);
+           
             db.carta_vida.findOne({ where: { id_participante: -1 } })
                 .then(data => {
                    return db.carta_vida.create({ texto:texto,id_participante:id_participante,remetente:nome})

3129c4a0eb7e946353ba0eff9c03314b9e0e233a
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-11-21 15:34:07 -0300

    Update User.cjs

--- src/models/User.cjs
+++ src/models/User.cjs
@@ -1,15 +1,34 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
-    nome: DataTypes.STRING,   
-    email: DataTypes.STRING,  
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    address: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
     role: DataTypes.STRING,
     verify: DataTypes.BOOLEAN,
-    password: DataTypes.STRING, 
+    password: DataTypes.STRING,
+    id_cargo: DataTypes.INTEGER,
+    id_area: DataTypes.INTEGER,
+    id_setor: DataTypes.INTEGER,
+    id_distrito: DataTypes.INTEGER,
+    status: DataTypes.INTEGER,
+    url_file: DataTypes.STRING,
+    lider_celula: DataTypes.STRING,
   }, {});
 
-  user.associate = function(models) {    
-  
+  user.associate = function(models) {   
+    //models.user.hasMany(models.grupos, { foreignKey: 'id' });      
+    //   models.user.hasMany(models.user_cargo, {
+    //  foreignKey: 'id_cargo',
+    //  sourceKey: 'id',
+    //  as: 'user_cargo' });     
+    //models.user.belongsToMany(models.user_cargo, { as: 'user_cargo',foreignKey: 'id',sourceKey: 'id'});
+    models.user.belongsTo(models.cargo, {foreignKey: 'id_cargo' }); 
+    models.user.belongsTo(models.areas, {as:'user_area', foreignKey: 'id_area' });      
+    models.user.belongsTo(models.areas, {as:'user_distrito', foreignKey: 'id_distrito' });      
+    models.user.belongsTo(models.areas, {as:'user_setor', foreignKey: 'id_setor' });  
   };
 
   return user;
